e437949fecc7fab6f02fbd64465665a334ba71bb
Hieu Tran <trantrunghieu3010@gmail.com>
2020-07-08 12:39:28 +0700

    Support callback to get default select fields

--- lib/graphqlEntityFetcher.d.ts
+++ lib/graphqlEntityFetcher.d.ts
@@ -1,17 +1,17 @@
 import GraphQLDoorClient from './GraphQLDoorClient';
 import { QueryParams } from './types';
-export default class GraphQLEntityFetcher {
+export default class GraphQLEntityFetcher<T> {
     graphqlClient: GraphQLDoorClient;
     entityName: string;
-    defaultSelectFields: string[];
-    constructor(entityName: string, defaultSelectFields: string[], graphqlClient: GraphQLDoorClient);
-    queryManyAsync: (queryParams: QueryParams, selectFields?: string[] | undefined) => Promise<any>;
-    queryOneAsync: (queryParams: QueryParams, selectFields?: string[] | undefined) => Promise<any>;
-    getByIdAsync: (id: string, queryParams: QueryParams, selectFields?: string[] | undefined) => Promise<any>;
+    onGetSelectFields: () => string[];
+    constructor(entityName: string, onGetSelectFields: () => string[], graphqlClient: GraphQLDoorClient);
+    queryManyAsync: (queryParams: QueryParams, selectFields?: string[] | undefined) => Promise<T[]>;
+    queryOneAsync: (queryParams: QueryParams, selectFields?: string[] | undefined) => Promise<T>;
+    getByIdAsync: (id: string, queryParams: QueryParams, selectFields?: string[] | undefined) => Promise<T>;
     countAsync: (query: string) => Promise<number>;
-    addAsync: (model: any, selectFields?: string[] | undefined) => Promise<any>;
-    updateAsync: (id: string, model: any, selectFields?: string[] | undefined) => Promise<any>;
+    addAsync: (model: any, selectFields?: string[] | undefined) => Promise<T>;
+    updateAsync: (id: string, model: any, selectFields?: string[] | undefined) => Promise<T>;
     deleteAsync: (id: string) => Promise<unknown>;
     executeCustomMutationAsync: (mutationName: string, payload: any, variable: any, selectFields?: string[] | undefined) => Promise<any>;
-    addBatchAsync<T>(models: T[]): Promise<any>;
+    addBatchAsync(models: T[]): Promise<any>;
 }
--- src/graphqlEntityFetcher.ts
+++ src/graphqlEntityFetcher.ts
@@ -2,40 +2,50 @@ import camelCase from 'lodash/fp/camelCase';
 import GraphQLDoorClient from './GraphQLDoorClient';
 import { QueryParams } from './types';
 
-export default class GraphQLEntityFetcher {
+export default class GraphQLEntityFetcher<T> {
   graphqlClient: GraphQLDoorClient;
   entityName: string;
-  defaultSelectFields: string[];
+  onGetSelectFields: () => string[];
 
-  constructor(entityName: string, defaultSelectFields: string[], graphqlClient: GraphQLDoorClient) {
+  constructor(entityName: string, onGetSelectFields: () => string[], graphqlClient: GraphQLDoorClient) {
     this.entityName = camelCase(entityName);
-    this.defaultSelectFields = defaultSelectFields;
     this.graphqlClient = graphqlClient;
+    this.onGetSelectFields = onGetSelectFields;
   }
 
-  queryManyAsync = (queryParams: QueryParams, selectFields?: string[]) =>
-    this.graphqlClient.queryManyAsync(this.entityName, queryParams, selectFields || this.defaultSelectFields);
+  queryManyAsync = (queryParams: QueryParams, selectFields?: string[]): Promise<T[]> => {
+    const defaultSelectFields = this.onGetSelectFields();
+    return this.graphqlClient.queryManyAsync(this.entityName, queryParams, selectFields || defaultSelectFields);
+  };
 
-  queryOneAsync = (queryParams: QueryParams, selectFields?: string[]) =>
-    this.graphqlClient.queryOneAsync(this.entityName, queryParams, selectFields || this.defaultSelectFields);
+  queryOneAsync = (queryParams: QueryParams, selectFields?: string[]): Promise<T> => {
+    const defaultSelectFields = this.onGetSelectFields();
+    return this.graphqlClient.queryOneAsync(this.entityName, queryParams, selectFields || defaultSelectFields);
+  };
 
-  getByIdAsync = (id: string, queryParams: QueryParams, selectFields?: string[]) =>
-    this.graphqlClient.getByIdAsync(this.entityName, id, queryParams, selectFields || this.defaultSelectFields);
+  getByIdAsync = (id: string, queryParams: QueryParams, selectFields?: string[]): Promise<T> => {
+    const defaultSelectFields = this.onGetSelectFields();
+    return this.graphqlClient.getByIdAsync(this.entityName, id, queryParams, selectFields || defaultSelectFields);
+  };
 
   countAsync = (query: string): Promise<number> => this.graphqlClient.countAsync(this.entityName, query);
 
-  addAsync = (model: any, selectFields?: string[]) =>
-    this.graphqlClient.addAsync(this.entityName, model, selectFields || this.defaultSelectFields);
+  addAsync = (model: any, selectFields?: string[]): Promise<T> => {
+    const defaultSelectFields = this.onGetSelectFields();
+    return this.graphqlClient.addAsync(this.entityName, model, selectFields || defaultSelectFields);
+  };
 
-  updateAsync = (id: string, model: any, selectFields?: string[]) =>
-    this.graphqlClient.updateAsync(this.entityName, id, model, selectFields || this.defaultSelectFields);
+  updateAsync = (id: string, model: any, selectFields?: string[]): Promise<T> => {
+    const defaultSelectFields = this.onGetSelectFields();
+    return this.graphqlClient.updateAsync(this.entityName, id, model, selectFields || defaultSelectFields);
+  };
 
   deleteAsync = (id: string) => this.graphqlClient.deleteAsync(this.entityName, id);
 
   executeCustomMutationAsync = (mutationName: string, payload: any, variable: any, selectFields?: string[]) =>
     this.graphqlClient.executeCustomMutationAsync(this.entityName, mutationName, payload, variable, selectFields);
 
-  addBatchAsync<T>(models: T[]) {
+  addBatchAsync(models: T[]) {
     return this.graphqlClient.addBatchAsync<T>(this.entityName, models);
   }
 }
